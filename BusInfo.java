bef4b562b287401a0c578e0029291356d8af0992
Ynlekha1 <ynlekha6@gmail.com>
2024-10-12 13:17:13 +0530

    object

---
+++ BusInfo.java
@@ -0,0 +1,184 @@
+public class BusInfo {
+
+	int BusNumber;
+	String route;
+	int capacity;
+	String type;
+	int currentPassengers;
+
+public static void main (String [] args)
+{
+	Bus bus = new Bus();
+	bus.BusNumber = 17;
+	bus.route = "statebank to kunjathbail";
+	bus.capacity = 50;
+	bus.type = "regular";
+	bus.currentPassengers = 30;
+
+
+	Bus bus1 = new Bus();
+	bus1.BusNumber = 101;
+	bus1.route = "city center to nantoor junction";
+	bus1.capacity = 60;
+	bus1.type = "standard";
+	bus1.currentPassengers = 34;
+
+
+	Bus bus2 = new Bus();
+	bus2.BusNumber = 45;
+	bus2.route = "udupi to karkala";
+	bus2.capacity = 70;
+	bus2.type = "express";
+	bus2.currentPassengers = 37;
+
+
+	Bus bus3 = new Bus();
+	bus3.BusNumber = 103;
+	bus3.route = "airport to city";
+	bus3.capacity = 50;
+	bus3.type = "shuttle";
+	bus3.currentPassengers = 20;
+
+
+	Bus bus4 = new Bus();
+	bus4.BusNumber = 204;
+	bus4.route = "school route";
+	bus4.capacity = 50;
+	bus4.type = "school";
+	bus4.currentPassengers = 45;
+
+
+	Bus bus5 = new Bus();
+	bus5.BusNumber = 40;
+	bus5.route = "sarjapur to kempegowda airport";
+	bus5.capacity = 70;
+	bus5.type = "luxuary";
+	bus5.currentPassengers = 25;
+
+
+	Bus bus6 = new Bus();
+	bus6.BusNumber = 202;
+	bus6.route = "city tour";
+	bus6.capacity = 30;
+	bus6.type = "tourist";
+	bus6.currentPassengers = 20;
+
+
+
+	Bus bus7 = new Bus();
+	bus7.BusNumber = 435;
+	bus7.route = "city to beach resort";
+	bus7.capacity = 30;
+	bus7.type = "coach bus";
+	bus7.currentPassengers = 10;
+
+
+	Bus bus8 = new Bus();
+	bus8.BusNumber = 207;
+	bus8.route = "cit A to city B";
+	bus8.capacity = 80;
+	bus8.type = "double decker";
+	bus8.currentPassengers = 60;
+
+
+	Bus bus9 = new Bus();
+	bus9.BusNumber = 207;
+	bus9.route = "bengaluru to manglore";
+	bus9.capacity = 70;
+	bus9.type = "sleeper";
+	bus9.currentPassengers = 35;
+
+
+
+
+
+
+
+
+
+
+    System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus.BusNumber);
+	System.out.println("Route :"+bus.route);
+	System.out.println("Capacity :"+bus.capacity);
+	System.out.println("Type :"+bus.type);
+	System.out.println("current passengers :"+bus.currentPassengers);
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus1.BusNumber);
+	System.out.println("Route :"+bus1.route);
+	System.out.println("Capacity :"+bus1.capacity);
+	System.out.println("Type :"+bus1.type);
+	System.out.println("current passengers :"+bus1.currentPassengers);
+
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus2.BusNumber);
+	System.out.println("Route :"+bus2.route);
+	System.out.println("Capacity :"+bus2.capacity);
+	System.out.println("Type :"+bus2.type);
+	System.out.println("current passengers :"+bus2.currentPassengers);
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus3.BusNumber);
+	System.out.println("Route :"+bus3.route);
+	System.out.println("Capacity :"+bus3.capacity);
+	System.out.println("Type :"+bus3.type);
+	System.out.println("current passengers :"+bus3.currentPassengers);
+
+
+
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus4.BusNumber);
+	System.out.println("Route :"+bus4.route);
+	System.out.println("Capacity :"+bus4.capacity);
+	System.out.println("Type :"+bus4.type);
+	System.out.println("current passengers :"+bus4.currentPassengers);
+
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus5.BusNumber);
+	System.out.println("Route :"+bus5.route);
+	System.out.println("Capacity :"+bus5.capacity);
+	System.out.println("Type :"+bus5.type);
+	System.out.println("current passengers :"+bus5.currentPassengers);
+
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus6.BusNumber);
+	System.out.println("Route :"+bus6.route);
+	System.out.println("Capacity :"+bus6.capacity);
+	System.out.println("Type :"+bus6.type);
+	System.out.println("current passengers :"+bus6.currentPassengers);
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus7.BusNumber);
+	System.out.println("Route :"+bus7.route);
+	System.out.println("Capacity :"+bus7.capacity);
+	System.out.println("Type :"+bus7.type);
+	System.out.println("current passengers :"+bus7.currentPassengers);
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus8.BusNumber);
+	System.out.println("Route :"+bus8.route);
+	System.out.println("Capacity :"+bus8.capacity);
+	System.out.println("Type :"+bus8.type);
+	System.out.println("current passengers :"+bus8.currentPassengers);
+
+	System.out.println("-----------------------------------");
+	System.out.println("Bus number is :"+bus9.BusNumber);
+	System.out.println("Route :"+bus9.route);
+	System.out.println("Capacity :"+bus9.capacity);
+	System.out.println("Type :"+bus9.type);
+	System.out.println("current passengers :"+bus9.currentPassengers);
+
+	System.out.println("-----------------------------------");
+
+
+
+
+
+}
+	
+}
