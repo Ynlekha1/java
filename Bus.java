c190ba020753fbdd72442ba3fdeacd74f3267232
Ynlekha1 <ynlekha6@gmail.com>
2024-10-12 13:19:42 +0530

    states and behavior

---
+++ Bus.java
@@ -0,0 +1,25 @@
+public class Bus{
+
+	int BusNumber;
+	String route;
+	int capacity;
+	String type;
+	int currentPassengers;
+
+public static void start()
+{
+
+}
+public static void steering()
+{
+
+}
+public static void acceleration()
+{
+
+}
+public static void brake()
+{
+
+}
+}
